380759f1bded18b30215bbef33ae4b105d1fab21
rouzbeh <rouzwawi@gmail.com>
2011-10-19 21:18:04 +0200

    mikoni indication

--- public/js/mikoni.js
+++ public/js/mikoni.js
@@ -1,18 +1,24 @@
 var url = 'http://www.mikoni.se/';
+var did = false;
 function postMikoni(action, title)
 {
+	if (did) return;
+	did = true;
+
 	FB.api('/me/mikonam:' + action + '?chi=' + url + title,'post',
-			function(response) {
-				if (!response || response.error) {
-					alert('Try logging it with Facebook first');
-				} else {
-					//alert('Post was successful! Action ID: ' + response.id);
-					$.ajax({
-						url: '/' + title + "/touch",
-						type: 'put'
-					})
-				}
-			});
+	function(response) {
+		if (!response || response.error) {
+			alert('Try logging it with Facebook first');
+			did = false;
+		} else {
+			//alert('Post was successful! Action ID: ' + response.id);
+			$('.did.hide').show('slow');
+			$.ajax({
+				url: '/' + title + "/touch",
+				type: 'put'
+			})
+		}
+	});
 }
 function mikoni(title) { postMikoni('mikoni', title); }
 function concur(title) { postMikoni('concur', title); }
